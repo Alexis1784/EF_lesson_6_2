79a880b4ed33a7a16f72774ea56a2bfd416a6089
Alex <alexis1784@gmail.com>
2016-01-28 15:35:09 +0500

    have added one-to-one binding

--- EF_lesson_6_2/MyContext.cs
+++ EF_lesson_6_2/MyContext.cs
@@ -20,11 +20,25 @@ namespace EF_lesson_6_2
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Phone>()
-                .HasRequired(c => c.Company)
-                .WithOptional(c => c.BestSeller);
+
+            // связь один-к нулю или ко многим
+            //modelBuilder.Entity<Phone>()
+            //    .HasRequired(c => c.Company)
+            //    .WithOptional(c => c.BestSeller);
+
+            // Связь один-к-одному (One-to-One)
+            //modelBuilder.Entity<Phone>()
+            //    .HasRequired(c => c.Company)
+            //    .WithRequiredPrincipal(c => c.BestSeller);
+            // вариант выше не работает. Выдает ошибку.
+            //или так
+            modelBuilder.Entity<Company>()
+                .HasRequired(c => c.BestSeller)
+                .WithRequiredPrincipal(c => c.Company);
 
             base.OnModelCreating(modelBuilder);
+
+
         }
     }
 }
